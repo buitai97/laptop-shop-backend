2e21afac4ba8d7feafd8ecd6ad989a75ef513793
taibui97 <taibui97@outlook.com>
2025-08-27 16:17:13 -0700

    remove ignore files

--- dist/services/user.service.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handleCreateUser = void 0;
-const handleCreateUser = (name, email, address) => {
-    console.log(">>> insert a new user");
-};
-exports.handleCreateUser = handleCreateUser;
-//# sourceMappingURL=user.service.js.map
