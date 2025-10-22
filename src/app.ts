086ff9ea0f9979bd3baa52c05d94550fdec355af
taibui97 <taibui97@outlook.com>
2025-10-22 16:06:58 -0700

    remove vercel config

--- src/app.ts
+++ src/app.ts
@@ -52,12 +52,9 @@ if (process.env.NODE_ENV === 'development' && !process.env.VERCEL) {
     initDatabase();
 }
 
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-}
-//app.use((_req, res) => res.status(404).render('status/404.ejs'))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
 
 export default app
